7c9e6cc7a1411043af2ab61a882b2c2dc8d983fc
Drozerd <debeneyf@gmail.com>
2022-12-21 10:23:34 +0100

    Ajout ClientController

---
+++ src/main/java/com/inti/process/ProcessGetAllClient.java
@@ -0,0 +1,18 @@
+//package com.inti.process;
+//
+//import org.apache.camel.Exchange;
+//import org.apache.camel.Processor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//
+//
+//public class ProcessGetAllClient implements Processor {
+//
+//	@Override
+//	public void process(Exchange exchange) throws Exception {
+//		exchange.getIn().getBody();
+//		
+//		
+//	}
+//
+//}
