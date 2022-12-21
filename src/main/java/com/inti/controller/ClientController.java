7b8f4628301b5e55c14494e6142327fc25ed2b7c
Drozerd <debeneyf@gmail.com>
2022-12-21 09:47:45 +0100

    kjln:ixdfgkj:bxdgrnjkl:

--- src/main/java/com/inti/controller/ClientController.java
+++ src/main/java/com/inti/controller/ClientController.java
@@ -1,10 +1,13 @@
 package com.inti.controller;
 
+import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
 	
+	@Autowired
+	ProducerTemplate producerTemplate;
 	
 }
